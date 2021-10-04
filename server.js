f33ee5bc205eb719c9a5b5e591c2859d2ecb2fc3
Elmer Mendoza <mdelefm@gmail.com>
2021-10-03 21:46:58 -0600

    12th message

--- server.js
+++ server.js
@@ -11,16 +11,6 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express();
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(_dirname,"/client/build")));
-  app.get("*",(req,res)=> {
-    res.sendFile(path.join(_dirname,"client","build","index.html"));
-  })
-} else {
-  app.get("/",(req,res)=> {
-    res.send("Api running")
-  })
-}
 
 app.use(cors());
 
@@ -31,14 +21,26 @@ app.use(express.json());
 // Connect to Mongo
 
 mongoose
-    .connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>console.log('MongoDB connected...'))
-    .catch(err=>console.log(err));
+.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>console.log('MongoDB connected...'))
+.catch(err=>console.log(err));
 
- 
+
 // Use Route
 
 app.use('/api/reviews',reviewRoutes)
+
+// code added
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname,"/client/build")));
+  app.get("*",(req,res)=> {
+    res.sendFile(path.join(_dirname,"client","build","index.html"));
+  })
+} else {
+  app.get("/",(req,res)=> {
+    res.send("Api running")
+  })
+}
 
 const port = process.env.PORT || 5001;
 
