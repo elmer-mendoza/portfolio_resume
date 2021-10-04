368be5f16126c15e469d8a0c0fdded5820ffc199
Elmer Mendoza <mdelefm@gmail.com>
2021-10-03 21:22:06 -0600

    11th message

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ const app = express();
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(_dirname,'/client/build')));
+  app.use(express.static(path.join(_dirname,"/client/build")));
+  app.get("*",(req,res)=> {
+    res.sendFile(path.join(_dirname,"client","build","index.html"));
+  })
+} else {
+  app.get("/",(req,res)=> {
+    res.send("Api running")
+  })
 }
 
 app.use(cors());
