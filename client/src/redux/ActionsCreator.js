355126893376cf83273533489800a7f1132c106f
Elmer Mendoza <mdelefm@gmail.com>
2021-10-04 09:05:08 -0600

    16th messaage

--- client/src/redux/ActionsCreator.js
+++ client/src/redux/ActionsCreator.js
@@ -1,11 +1,10 @@
 import * as ActionTypes from './ActionTypes';
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 export  const fetchReviews=()=>async(dispatch) => {
+        // const REVIEWDATA_URL=process.env.REACT_APP_REVIEWDATA_URL;
+        // const response= await fetch(process.env.REACT_APP_REVIEWDATA_URL);
         const REVIEWDATA_URL=process.env.REACT_APP_REVIEWDATA_URL;
-        const response= await fetch(process.env.REACT_APP_REVIEWDATA_URL);
+        const response= await fetch(REVIEWDATA_URL);
         const fetchedreviews = await response.json();
         const sortedReviewsByDate=fetchedreviews.sort((a,b)=> a.date>b.date ? -1:1);
         dispatch(addReviews(sortedReviewsByDate));
@@ -19,7 +18,7 @@ export  const fetchReviews=()=>async(dispatch) => {
 export const fetchData = () => dispatch => {
      dispatch(dataLoading());
       const MAINDATA_URL=process.env.REACT_APP_MAINDATA_URL;
-    return fetch(process.env.REACT_APP_MAINDATA_URL)
+    return fetch(MAINDATA_URL)
     .then(response => {
          if (response.ok) {
             return response;
