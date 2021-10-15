b8d294edbcb19fae93d2979c6ba490eb557e1dc7
Elmer Mendoza <mdelefm@gmail.com>
2021-10-15 14:17:26 -0600

    11 mess

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 5001;
 
 app.listen(port,()=> console.log(`Server started on port ${port}`));
